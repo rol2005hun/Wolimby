41eb477505322aa1f3dbd5c854b709a36f49122b
rol2005hun <81093097+rol2005hun@users.noreply.github.com>
2023-04-03 22:10:43 +0300

    Update auth.global.ts

--- client/middleware/auth.global.ts
+++ client/middleware/auth.global.ts
@@ -1,10 +1,5 @@
 import { userStore } from '@/store';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    await userStore.getCurrentUser();
-    if(!userStore.isLoggedIn && to.name != 'auth') {
-        return navigateTo('/auth');
-    } else if(userStore.isLoggedIn && to.name == 'auth') {
-        return navigateTo('/access');
-    }
+    
 });
