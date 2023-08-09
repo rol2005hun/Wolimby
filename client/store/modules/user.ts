b82b6ee36f155b3ebe643b6088152d3bcd19c951
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-08-10 02:15:34 +0300

    das

--- client/store/modules/user.ts
+++ client/store/modules/user.ts
@@ -95,7 +95,7 @@ export const useUserStore = defineStore('user', {
       try {
         await functions.deleteCookie('token');
         delete axios.defaults.headers.common['Authorization'];
-        return navigateTo('https://account.wolimby.hu/auth', { external: true });
+        return await navigateTo('https://account.wolimby.hu/auth', { external: true });
       } catch(err: any) {
         this.$state.error = err.response.data.message;
       }
