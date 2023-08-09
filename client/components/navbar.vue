fcf7ee39e5922eff9c9cd321a3bda9d7223037c4
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-08-09 22:21:58 +0300

    Update navbar.vue

--- client/components/navbar.vue
+++ client/components/navbar.vue
@@ -5,7 +5,7 @@
       <ul class="nav-menu">
         <li class="nav-item">
           <a v-if="isLoggedIn && isOnline" href="#" class="nav-link" @click="toggleMenu(2)">{{ currentUser.profile.username }}</a>
-          <a v-else :href="'account.' + functions.getDomain() + '/auth'" class="nav-link">Bejelentkezés</a>
+          <a v-else :href="'account.' + functions.getDomain() + '/auth?redirectTo=' + useRequestURL().href" class="nav-link">Bejelentkezés</a>
           <ul v-if="isLoggedIn && isOnline" class="nav-submenu">
             <li class="nav-submenu-link"><a :href="'account.' + functions.getDomain() + '/profile'">Profilom</a></li>
             <li class="nav-submenu-link"><a :href="'account.' + functions.getDomain() + '/settings'">Beállítások</a></li>
