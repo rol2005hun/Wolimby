9205def6628c6ae44f9c9bfa79585e7df6633495
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-08-10 18:14:55 +0300

    dd

--- client/middleware/auth.global.ts
+++ client/middleware/auth.global.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { userStore, postStore, testStore } from '@/store';
-import functions from '@/assets/ts/functions';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   await testStore.testApi();
-  const path: string = `https://account.${functions.getDomain}/auth?redirectTo=${useRequestURL().href}`;
   if(testStore.isOnline) {
     const token = useCookie('token').value;
     if (token) {
@@ -15,11 +13,11 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     }
 
     if (!token) {
-      return navigateTo(path, { external: true });
+      return navigateTo('https://account.wolimby.hu/auth?redirectTo=' + useRequestURL().href, { external: true });
     }
 
     if(to.name == undefined) return navigateTo('/home');
   } else {
-    return navigateTo(path, { external: true });
+    return navigateTo('https://account.wolimby.hu/auth?redirectTo=' + useRequestURL().href, { external: true });
   }
 });
