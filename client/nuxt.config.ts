288401d4bcdb9b68126ed455a6a055723e92b74a
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-11-23 18:06:19 +0200

    Update nuxt.config.ts

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -26,7 +26,7 @@ const config: NuxtConfig = {
   buildModules: ['@pinia/nuxt'],
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:2000',
+      apiBase: 'https://api.account.wolimby.hu',
       imgurClientId: '80af936d45232db'
     }
   }
