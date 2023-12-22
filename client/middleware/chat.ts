5b7305acbc9a69f542dea43d5fc80386aaa2df53
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-12-23 01:11:20 +0200

    Delete client/middleware directory

--- client/middleware/chat.ts
+++
@@ -1,6 +0,0 @@
-import { userStore, chatStore } from '@/store';
-
-export default defineNuxtRouteMiddleware(async (to, from) => {
-    await userStore.getUsers();
-    await chatStore.getChats(userStore.currentUser._id);
-});
--- client/middleware/post.ts
+++
@@ -1,5 +0,0 @@
-import { postStore } from '@/store';
-
-export default defineNuxtRouteMiddleware(async (to, from) => {
-    await postStore.getPost(to.params.id as string);
-});
