98ff7fd6f09ed2741d3474a80ca6e36f472b4ce8
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-12-24 18:31:22 +0200

    Update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,7 +2,7 @@
     <div class="home-header">
         <div class="sections">
             <div class="left-section">
-                <h1>Ismerd meg a Wolimby nyújtotta élményt! -old</h1>
+                <h1>Ismerd meg a Wolimby nyújtotta élményt!</h1>
                 <h3>A Wolimby egy olyan oldal, ahol ingyenes és reklámmentesen használhatsz különböző dolgokat, és ehhez csak annyit kell megtegyél, hogy bejelentkezel
                 vagy beregisztrálsz az oldalra, majd élvezed az élményt.
                 </h3>
