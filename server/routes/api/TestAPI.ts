48731390d5cf327b31c037a370f94d98aa11a60d
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2024-01-02 22:02:33 +0200

    Delete server directory

--- server/routes/api/TestAPI.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controller from '../../controllers/TestController';
-
-const router = express.Router();
-
-router.get('/testrequest', controller.test);
-
-export = router;
--- server/routes/api/UsersAPI.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import controller from '../../controllers/UserController';
-import { isLoggedIn } from '../../middlewares/UserMiddleware';
-
-const router = express.Router();
-
-router.get('/currentuser', isLoggedIn, controller.currentUser);
-
-export = router;
